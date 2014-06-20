828b95ed6aff9f099b44e7604932ff667180c071
Rob Lowcock <Rob.Lowcock@Miniclip.com>
2014-06-20 09:06:06 +0100

    Added MongoDB test

--- Architect/Test.php
+++ Architect/Test.php
@@ -6,6 +6,18 @@ class Test extends ArchitectAbstract
 {
 	public function test2()
 	{
-		return array("Hi there");
+		$m = new \MongoClient();
+		$db = $m->{'architect-dev'};
+
+		$collection = $db->projects;
+		$cursor = $collection->find();
+
+		$output = array();
+
+		foreach ($cursor as $doc) {
+			$output[] = $doc;
+		}
+
+		return $output;
 	}
 }
