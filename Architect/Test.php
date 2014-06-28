7acccc67ef4e0f15af746358f73bdcac2cf671da
BobLoco <rob.lowcock@gmail.com>
2014-06-28 22:42:44 +0100

    Removed mongodb test

--- Architect/Test.php
+++ Architect/Test.php
@@ -6,18 +6,6 @@ class Test extends ArchitectAbstract
 {
 	public function test2()
 	{
-		$m = new \MongoClient();
-		$db = $m->{'architect-dev'};
-
-		$collection = $db->projects;
-		$cursor = $collection->find();
-
-		$output = array();
-
-		foreach ($cursor as $doc) {
-			$output[] = $doc;
-		}
-
-		return $output;
+		return array("Hello there!");
 	}
 }
