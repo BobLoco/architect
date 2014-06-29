4dff9353dd3ac692900c9a5b16688e01a445d0dc
BobLoco <rob.lowcock@gmail.com>
2014-06-29 13:53:32 +0100

    Added router

---
+++ Architect/Router.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Architect;
+
+class Router extends ArchitectAbstract
+{
+	public function routes($app)
+	{
+		$app->get('/:class/:function', function($class, $function) use ($app) {
+			$fullclass = '\\Architect\\' . ucfirst($class);
+			$loaded = new $fullclass();
+
+			$loaded->setParams($_GET);
+
+			$app->contentType('application/json');
+
+			echo json_encode($loaded->$function());
+		});
+	}
+}
