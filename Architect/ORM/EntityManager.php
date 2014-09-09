76dbf94faed99aae8dea2f21da7a9bf8027e2666
Rob Lowcock <Rob.Lowcock@Miniclip.com>
2014-09-09 08:08:17 +0100

    Removed password details

---
+++ Architect/ORM/EntityManager.php
@@ -0,0 +1,26 @@
+<?php
+namespace Architect\ORM;
+
+// use the Doctrine classes
+use \Doctrine\ORM\Tools\Setup;
+
+class EntityManager extends \Architect\ArchitectAbstract
+{
+	public function createManager()
+	{
+		// setup Doctrine
+		$dev_mode = true;
+		$config = Setup::createAnnotationMetadataConfiguration(array(__DIR__.'/src'), $dev_mode);
+
+		// connect to the DB
+		$conn = array(
+			'driver' => Config::DB_DRIVER,
+			'user' => Config::DB_USER,
+			'password' => Config::DB_PASS,
+			'dbname' => Config::DB_DATABASE,
+		);
+
+		// get the entity manager
+		return \Doctrine\ORM\EntityManager::create($conn, $config);
+	}
+}
