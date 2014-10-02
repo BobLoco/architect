bb2f8991691f72e6aba5e6ec793410a83e0c56ce
Rob Lowcock <Rob.Lowcock@Miniclip.com>
2014-10-02 18:12:28 +0100

    Started to try and add secret to composer package

--- Architect/Access.php
+++ Architect/Access.php
@@ -3,14 +3,22 @@
 namespace Architect;
 
 class Access {
-
 	const MASTER = '1392efdc4a7dd3808f33940250f624fd';
 
 	public function validateRequest()
 	{
+		if (empty($_REQUEST['secret'])) {
+			throw new Exception('No application secret set');
+		}
+
+		$secret = $_REQUEST['secret'];
+
 		if ($secret == self::MASTER) {
 			return true;
 		} else {
+			$entity_manager = new EntityManager();
+			$orm = $entity_manager->createManager();
+
 			$app_id;
 		}
 	}
